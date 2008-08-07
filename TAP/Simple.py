57b301dc3909b7f73c70e4e6bd33726fd3ce6f93
Ricardo SIGNES <rjbs@cpan.org>
2008-08-07 18:13:52 -0400

    new terms

--- TAP/Simple.py
+++ TAP/Simple.py
@@ -12,26 +12,26 @@ ok = builder.ok
 def diag(comment):
   print "# %s" % re.compile("\n(.)").sub(comment, '\n#\1')
 
-def eq_ok(got, expected, comment):
-  okness = got == expected
+def eq_ok(have, want, comment):
+  okness = have == want
   ok(okness, comment)
   if not okness:
-    diag("expected: %s" % expected)
-    diag("     got: %s" % got)
+    diag("want: %s" % want)
+    diag("have: %s" % have)
   return okness
 
-def is_ok(got, expected, comment):
-  okness = got is expected
+def is_ok(have, want, comment):
+  okness = have is want
   ok(okness, comment)
   if not okness:
-    diag("expected id: %s" % id(expected))
-    diag("     got id: %s" % id(got))
+    diag("want id: %s" % id(want))
+    diag("have id: %s" % id(have))
   return okness
 
 def isa_ok(object, cls, object_name="the object"):
   okness = isinstance(object, cls)
   ok(okness, object_name + " is a " + repr(cls))
   # if not okness:
-  #   diag("expected id: %s" % id(expected))
-  #   diag("     got id: %s" % id(got))
+  #   diag("want id: %s" % id(want))
+  #   diag("have id: %s" % id(have))
   return okness
