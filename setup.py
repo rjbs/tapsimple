a8eddb61afca624238267dc6ecac68d8e75f921b
cattail <zhongchiyu@gmail.com>
2015-08-05 11:29:29 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
   name    = "pyTAP",
   version = "0.001",
-  py_modules = find_packages(),
+  py_modules = ["TAP.simple"],
 )
